ba5c72c387bfe246d94e9ddcbcb3dfa6fbcfb74e
Jacob Sapir <jacob.sapir@realtechnologytools.com>
2020-07-21 17:00:11 +0200

    Add useTheClock.m and cheatsheet.pdf.

---
+++ fileMan/useTheClock.m
@@ -0,0 +1,7 @@
+c = clock;
+y = num2str(c(1)); % year
+m = num2str(c(2)); % month
+d = num2str(c(3)); % day
+h = num2str(c(4)); % hour
+m = num2str(c(5)); % minute
+flNm = ['report_' m '.' d '.' y '@' h 'h' m 'm']
